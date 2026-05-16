8ca4fec80748eb8b76ebf30ac1ac3fa528f4627c
Neltharion <NtNeltharion@gmail.com>
2026-05-16 13:29:34 +0300

    Correctness fix Spaces should never have been encoded as "+" in url encoding, now there are separate functions for url and form encoding

--- src/lib.rs
+++ src/lib.rs
@@ -60,19 +60,10 @@ impl Bitmask {
     pub const PATH: Bitmask = Bitmask::URI_COMPONENT.add(b'/');
 }
 
-/// Encodes given slice using provided [`Bitmask`]
-/// # Example
-/// ```
-/// # use percent_encoding_lite::Bitmask;
-/// let string = "Dragonborn, dragonborn, by his honor is sworn";
-/// let encoded = percent_encoding_lite::encode(string, Bitmask::URI);
-/// assert_eq!(&encoded, "Dragonborn,+dragonborn,+by+his+honor+is+sworn");
-/// ```
-pub fn encode(src: impl AsRef<[u8]>, mask: Bitmask) -> String {
-    let src = src.as_ref();
+fn encode_inner(src: &[u8], mask: Bitmask, is_formdata: bool) -> String {
     let mut out = String::with_capacity(src.len());
     for &ch in src.iter() {
-        if ch == b' ' {
+        if ch == b' ' && !mask.contains(b' ') && is_formdata {
             out.push('+');
         } else if mask.contains(ch) {
             out.push(ch as char);
@@ -86,18 +77,35 @@ pub fn encode(src: impl AsRef<[u8]>, mask: Bitmask) -> String {
     out
 }
 
-/// Decodes a percent encoded string
+/// Encodes given slice using provided [`Bitmask`]
 /// # Example
 /// ```
-/// let encoded = "%54%6F%20%6B%65%65%70%20%65%76%69%6C%20%66%6F%72%65%76%65%72%20%61%74%20%62%61%79%21";
-/// let decoded = percent_encoding_lite::decode(encoded);
-/// assert_eq!(&decoded, b"To keep evil forever at bay!");
+/// # use percent_encoding_lite::Bitmask;
+/// let string = "Till the heat death of the universe, we will suffer";
+/// let encoded = percent_encoding_lite::encode(string, Bitmask::URI_COMPONENT);
+/// assert_eq!(&encoded, "Till%20the%20heat%20death%20of%20the%20universe%2C%20we%20will%20suffer");
 /// ```
-pub fn decode(src: impl AsRef<[u8]>) -> Vec<u8> {
-    let mut iter = src.as_ref().iter();
+pub fn encode(src: impl AsRef<[u8]>, mask: Bitmask) -> String {
+    encode_inner(src.as_ref(), mask, false)
+}
+
+/// Same as [`encode`], but replaces spaces with the `+` symbol
+/// # Example
+/// ```
+/// # use percent_encoding_lite::Bitmask;
+/// let string = "what happens when you type google in google";
+/// let encoded = percent_encoding_lite::encode_form(string, Bitmask::URI_COMPONENT);
+/// assert_eq!(&encoded, "what+happens+when+you+type+google+in+google");
+/// ```
+pub fn encode_form(src: impl AsRef<[u8]>, mask: Bitmask) -> String {
+    encode_inner(src.as_ref(), mask, true)
+}
+
+fn decode_inner(src: &[u8], is_formdata: bool) -> Vec<u8> {
+    let mut iter = src.iter();
     let mut out = vec![];
     while let Some(&i) = iter.next() {
-        if i == b'+' {
+        if i == b'+' && is_formdata {
             out.push(b' ');
         } else if i != b'%' {
             out.push(i);
@@ -114,18 +122,38 @@ pub fn decode(src: impl AsRef<[u8]>) -> Vec<u8> {
     out
 }
 
+/// Decodes a percent encoded string
+/// # Example
+/// ```
+/// let encoded = "%54%6F%20%6B%65%65%70%20%65%76%69%6C%20%66%6F%72%65%76%65%72%20%61%74%20%62%61%79%21";
+/// let decoded = percent_encoding_lite::decode(encoded);
+/// assert_eq!(&decoded, b"To keep evil forever at bay!");
+/// ```
+pub fn decode(src: impl AsRef<[u8]>) -> Vec<u8> {
+    decode_inner(src.as_ref(), false)
+}
+
+/// Same as [`decode`], but decodes `+` as space
+/// # Example
+/// ```
+/// let encoded = "Plus+is+space";
+/// let decoded = percent_encoding_lite::decode_form(encoded);
+/// assert_eq!(&decoded, b"Plus is space");
+/// ```
+pub fn decode_form(src: impl AsRef<[u8]>) -> Vec<u8> {
+    decode_inner(src.as_ref(), true)
+}
+
 /// Checks if this string contains any unencoded characters
 ///
-/// You can use this function to avoid double-encoding (when data has to be encoded, but may be encoded already)
+/// You can use this function to optimize your code, to avoid allocation if data does not need to he encoded (since this library's function don't return `Cow`)
 /// # Example
 /// ```
 /// # use percent_encoding_lite::{is_encoded, Bitmask};
-/// let string = "Dovahkiin, Dovahkiin, naal ok zin los vahriin, wah dein vokul mahfaeraak ahst vaal!";
-/// // Bitmask::URI_COMPONENT does not list ',' as allowed, so this string should be encoded
-/// assert!(is_encoded(&string, Bitmask::URI_COMPONENT) == false);
+/// let string = "Spaces should be encoded";
+/// assert_eq!(is_encoded(&string, Bitmask::URI_COMPONENT), false);
 /// ```
 pub fn is_encoded(src: impl AsRef<[u8]>, mask: Bitmask) -> bool {
-    let mask = mask.add(b'%').add(b'+');
     for &ch in src.as_ref() {
         if !mask.contains(ch) { return false; }
     }
@@ -138,37 +166,38 @@ mod test {
     #[test]
     fn urldecode_test() {
         let encoded = "Anno+1404.Gold+Edition.v+2.1.5010.0.(%D0%9D%D0%BE%D0%B2%D1%8B%D0%B9+%D0%94%D0%B8%D1%81%D0%BA).(2010).Repack";
-        let decoded = String::from_utf8(decode(encoded)).unwrap();
+        let decoded = String::from_utf8(decode_form(encoded)).unwrap();
         let correct = "Anno 1404.Gold Edition.v 2.1.5010.0.(Новый Диск).(2010).Repack";
         assert_eq!(&decoded, correct);
 
         let encoded = "The+Elder+Scrolls+V.+Skyrim.+Anniversary+Edition+v.1.6.640.0.8+(2011-2021)";
-        let decoded = String::from_utf8(decode(encoded)).unwrap();
+        let decoded = String::from_utf8(decode_form(encoded)).unwrap();
         let correct = "The Elder Scrolls V. Skyrim. Anniversary Edition v.1.6.640.0.8 (2011-2021)";
         assert_eq!(&decoded, correct);
     }
     #[test]
     fn urlencode_test() {
         let orig = "Microsoft Windows 10, version 22H2, build 19045.2846 (updated April 2023) - Оригинальные образы от Microsoft MSDN [Ru]";
-        let encoded = encode(orig, Bitmask::URI_COMPONENT);
+        let encoded = encode_form(orig, Bitmask::URI_COMPONENT);
         let correct = "Microsoft+Windows+10%2C+version+22H2%2C+build+19045.2846+(updated+April+2023)+-+%D0%9E%D1%80%D0%B8%D0%B3%D0%B8%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B5+%D0%BE%D0%B1%D1%80%D0%B0%D0%B7%D1%8B+%D0%BE%D1%82+Microsoft+MSDN+%5BRu%5D";
         assert_eq!(&encoded, correct);
 
         let orig = "Windows_Embedded_8.1_Industry_Pro_with_Update_x86_x64_MultiLang";
         let encoded = encode(orig, Bitmask::URI_COMPONENT);
         assert_eq!(&encoded, orig);
+        assert_eq!(is_encoded(&orig, Bitmask::URI_COMPONENT), true);
+
+        let orig = "In URI, space is %20";
+        let encoded = encode(orig, Bitmask::URI);
+        let correct = "In%20URI,%20space%20is%20%2520";
+        assert_eq!(&encoded, correct);
     }
     #[test]
     fn is_encoded_test() {
-        // No comma in Bitmask::URI_COMPONENT
-        assert!(is_encoded(",", Bitmask::URI_COMPONENT) == false);
-        // Regular text
-        assert!(is_encoded("abc", Bitmask::URI_COMPONENT) == true);
-        // No '[' in Bitmask::URI
-        assert!(is_encoded("abc[def", Bitmask::URI) == false);
-        // Bitmask::URI has comma, while Bitmask::URI_COMPONENT doesn't
-        assert!(is_encoded("abc,def", Bitmask::URI) == true);
-        // While not allowed, percent sign does not mean that string should be encoded
-        assert!(is_encoded("%01%02", Bitmask::URI) == true);
+        assert_eq!(is_encoded("abc", Bitmask::URI), true);
+        assert_eq!(is_encoded("abc,def", Bitmask::URI), true);
+        assert_eq!(is_encoded("abc,def", Bitmask::URI_COMPONENT), false);
+        assert_eq!(is_encoded("abc[def", Bitmask::URI), false);
+        assert_eq!(is_encoded("%01%02", Bitmask::URI), false);
     }
 }
